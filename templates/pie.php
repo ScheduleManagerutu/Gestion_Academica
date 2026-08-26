a84c343b6af67e9e105bebe39c4b01a96fd2c28b
ScheduleManagerutu <ScheduleManar.utu@gmail.com>
2026-08-26 12:38:16 -0300

    Información de contacto en pie.php

--- templates/pie.php
+++ templates/pie.php
@@ -3,7 +3,17 @@
  </div>  
                 </div>
 
-pie (de la pagina)
+<div class="card">
+       
+    <div class="card-body">
+        
+    <h8 class="card-title">Contacto:</h8>
+        
+        <a href="mailto:Schedulemanar.utu@gmail.com" class="card-link">Schedulemanar.utu@gmail.com</a>
+        
+    </div>
+</div>
+
 
 <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
