34f33794d9fa88607610656539a7fea7005cd658
ScheduleManagerutu <ScheduleManar.utu@gmail.com>
2026-09-04 08:36:52 -0300

    Base de datos guardadda totalmente en schema.sql

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,47 +1,110 @@
--- `BD Web`.Docente definition
+-- MySQL dump 10.13  Distrib 8.4.11, for Linux (x86_64)
+--
+-- Host: localhost    Database: BD Web
+-- ------------------------------------------------------
+-- Server version	8.4.11-0ubuntu0.26.04.1
 
-CREATE TABLE `Docente` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `Documento` varchar(20) NOT NULL,
-  `Materia` varchar(100) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB 
-DEFAULT CHARSET=utf8mb4 
-COLLATE=utf8mb4_0900_ai_ci 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-#---------------------------------------------
+--
+-- Table structure for table `Administrativo`
+--
 
-CREATE TABLE `BD Web`.Administrativo (
-	IDadministrativo INT auto_increment NOT NULL,
-	Nombre varchar(50) NOT NULL,
-	Apellido varchar(50) NOT NULL,
-	Correo varchar(100) NOT NULL,
-	Contraseña varchar(225) NOT NULL,
-	CONSTRAINT Administrativo_PK PRIMARY KEY (IDadministrativo),
-	CONSTRAINT Administrativo_UNIQUE UNIQUE KEY (Correo)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
-#--------------------------
-CREATE TABLE `BD Web`.Materia (
-	IDmateria INT auto_increment NOT NULL,
-	Nombre varchar(100) NOT NULL,
-	Horas_semana INT NOT NULL CHECK (Horas_semana >= 0),
-	CONSTRAINT Materia_PK PRIMARY KEY (IDmateria)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
-#------------------------
-CREATE TABLE `Usuario` (
-  `IDusuario` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(50) NOT NULL,
-  `Apellido` varchar(50) NOT NULL,
+DROP TABLE IF EXISTS `Administrativo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Administrativo` (
+  `IDadministrativo` int NOT NULL AUTO_INCREMENT,
   `Correo` varchar(100) NOT NULL,
-  PRIMARY KEY (`IDusuario`),
-  UNIQUE KEY `Usuario_UNIQUE` (`Correo`)
-) ENGINE=InnoDB 
-DEFAULT CHARSET=utf8mb4 
-COLLATE=utf8mb4_0900_ai_ci;
+  `Contraseña` varchar(225) NOT NULL,
+  `Usuario` varchar(100) NOT NULL,
+  PRIMARY KEY (`IDadministrativo`),
+  UNIQUE KEY `Administrativo_UNIQUE` (`Correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Administrativo`
+--
+
+LOCK TABLES `Administrativo` WRITE;
+/*!40000 ALTER TABLE `Administrativo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Administrativo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Docente`
+--
+
+DROP TABLE IF EXISTS `Docente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Docente` (
+  `IDdocente` int NOT NULL AUTO_INCREMENT,
+  `Documento` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Materia` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Telefono` varchar(20) DEFAULT NULL,
+  `Nombre` varchar(100) NOT NULL,
+  PRIMARY KEY (`IDdocente`),
+  CONSTRAINT `Docente_Materia_FK` FOREIGN KEY (`IDdocente`) REFERENCES `Materia` (`IDmateria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tabla de información docente';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Docente`
+--
+
+LOCK TABLES `Docente` WRITE;
+/*!40000 ALTER TABLE `Docente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Docente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Materia`
+--
+
+DROP TABLE IF EXISTS `Materia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Materia` (
+  `IDmateria` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(100) NOT NULL,
+  `Horas_semana` int NOT NULL,
+  `Turnos` varchar(10) NOT NULL,
+  PRIMARY KEY (`IDmateria`),
+  CONSTRAINT `Materia_chk_1` CHECK ((`Horas_semana` >= 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Materia`
+--
+
+LOCK TABLES `Materia` WRITE;
+/*!40000 ALTER TABLE `Materia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Materia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'BD Web'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-09-04  8:01:56
